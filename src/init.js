8c491db3e12938a23830151167d34f81252b661b
EkaterinaRina <catherinemarsiano@gmail.com>
2024-05-31 14:25:19 +0300

    fix lint bugs

--- src/init.js
+++ src/init.js
@@ -4,106 +4,101 @@ import axios from 'axios';
 import renderRSS from './view.js';
 
 const createUrl = (rssFeed) => {
-    const proxy = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
-    const proxiedUrl = `${proxy}${rssFeed}`;
-    return proxiedUrl;
+  const proxy = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
+  const proxiedUrl = `${proxy}${rssFeed}`;
+  return proxiedUrl;
 };
 
-const getParsedData = (data) => {
+export default async () => {
+  yup.setLocale({
+    mixed: {
+      default: 'default',
+      required: 'empty',
+      notOneOf: 'alreadyExists',
+    },
+    string: { url: 'invalidUrl' },
+  });
+
+  const state = {
+    form: {
+      status: 'feeling',
+      error: '',
+    },
+    links: [],
+    feeds: [],
+    posts: [],
+  };
+
+  const getParsedData = (data) => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(data, 'application/xml');
     const parseError = doc.querySelector('parsererror');
     if (parseError) {
-        return new Error('noRSS');
+      return new Error('noRSS');
     }
     return doc;
-};
+  };
 
-const getHTTPresponseData = (url) => axios.get(createUrl(url))
-    .then((response) =>  {
-        const parsedData = getParsedData(response.data.contents);
-        const feedsWithUrl = parsedData.feeds.map((feed) => ({ link: url, ...feed }));
-        state.feeds = [...feedsWithUrl, ...state.feeds];
+  const getHTTPresponseData = (url) => axios.get(createUrl(url))
+    .then((response) => {
+      const parsedData = getParsedData(response.data.contents);
+      const feedsWithUrl = parsedData.feeds.map((feed) => ({ link: url, ...feed }));
+      state.feeds = [...feedsWithUrl, ...state.feeds];
     })
     .catch(() => new Error('networkError'));
 
-
-const makeValidateScheme = (links) => {
+  const makeValidateScheme = (links) => {
     const schema = yup.string().notOneOf(links).url();
     return schema;
-};
+  };
 
-
-export default async () => {
-    yup.setLocale({
-        mixed: {
-          default: 'default',
-          required: 'empty',
-          notOneOf: 'alreadyExists',
+  const i18n = i18next.createInstance();
+  i18n.init({
+    lng: 'ru',
+    debug: true,
+    resources: {
+      ru: {
+        translation: {
+          validUrl: 'RSS успешно загружен',
+          invalidUrl: 'Ссылка должна быть валидным URL',
+          empty: 'Не должно быть пустым',
+          alreadyExists: 'RSS уже существует',
+          noRSS: 'Ресурс не содержит валидный RSS',
+          networkError: 'Ошибка сети',
+          default: 'Неизвестная ошибка. Что-то пошло не так',
         },
-        string: { url: 'invalidUrl' },
-      });
+      },
+    },
+  });
 
-    const state = {
-        form: {
-            status: 'feeling',
-            error: '',
-        },
-        links: [],
-        feeds: [],
-        posts: [],
-    };
+  const watchedState = renderRSS(state, i18n);
 
-    const i18n = i18next.createInstance();
-    i18n.init({
-        lng: 'ru',
-        debug: true,
-        resources: {
-        ru: {
-            translation: {
-            validUrl: 'RSS успешно загружен',
-            invalidUrl: 'Ссылка должна быть валидным URL',
-            empty: 'Не должно быть пустым',
-            alreadyExists: 'RSS уже существует',
-            noRSS: 'Ресурс не содержит валидный RSS',
-            networkError: 'Ошибка сети',
-            default: 'Неизвестная ошибка. Что-то пошло не так',
-            },
-        },
-        },
-    });
+  const rssForm = document.querySelector('.rss-form');
+  const input = document.querySelector('#url-input');
 
-    const watchedState = renderRSS(state, i18n);
+  rssForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    const rssForm = document.querySelector('.rss-form');
-    const input = document.querySelector('#url-input');
+    // const loadedFeed = Object.values(state.feeds).map((item) => item.link);
 
-    rssForm.addEventListener('submit', (e) => {
-        e.preventDefault();
+    makeValidateScheme(state.links).validate(input.value)
+      .then(() => {
+        getHTTPresponseData(input.value).then(() => {
+          state.form.error = i18n.t('validUrl');
+          console.log(state.feeds);
+          state.links.push(input.value);
+          watchedState.status = 'loaded';
+          watchedState.status = 'feeling';
+        }).catch(() => {
+          watchedState.form.error = i18n.t('noRSS');
+        });
+      })
 
-       // const loadedFeed = Object.values(state.feeds).map((item) => item.link);
-
-       makeValidateScheme(state.links).validate(input.value)
-       .then(() => {
-         getHTTPresponseData(input.value).then(() => {
-           state.form.error = i18n.t('validUrl');
-           console.log(state.feeds);
-           state.links.push(input.value);
-           watchedState.status = 'loaded';
-           watchedState.status = 'feeling';
-         }).catch(() => {
-            watchedState.form.error = i18n.t('noRSS');
-         })
-            
-        
-       })
-
-       .catch((error) => {
-        
+      .catch((error) => {
         const [currentError] = error.errors;
         watchedState.form.error = currentError;
         state.form.error = currentError;
-        console.log(state.form.error)
+        console.log(state.form.error);
       });
-    })
-}
+  });
+};
--- src/view.js
+++ src/view.js
@@ -1,35 +1,35 @@
 import onChange from 'on-change';
 
 const makeSuccesText = (input, p, text) => {
-    input.classList.remove('is-invalid');
-    input.classList.add('is-valid');
-    p.textContent = text;
-    p.classList.remove('text-danger');
-    p.classList.add('text-success');
+  input.classList.remove('is-invalid');
+  input.classList.add('is-valid');
+  p.textContent = text; // eslint-disable-next-line no-param-reassign
+  p.classList.remove('text-danger');
+  p.classList.add('text-success');
 };
 
 const makeInvaildText = (input, p, text) => {
-    input.classList.remove('is-valid');
-    input.classList.add('is-invalid');
-    p.classList.remove('text-success');
-    p.classList.add('text-danger');
-    p.textContent = text;
+  input.classList.remove('is-valid');
+  input.classList.add('is-invalid');
+  p.classList.remove('text-success');
+  p.classList.add('text-danger');
+  p.textContent = text; // eslint-disable-next-line no-param-reassign
 };
 
 export default (state, i18n) => onChange(state, (path, current) => {
-    const paragraph = document.querySelector('.feedback');
-    const urlInput = document.querySelector('#url-input');
+  const paragraph = document.querySelector('.feedback');
+  const urlInput = document.querySelector('#url-input');
 
-    switch (path) {
-        case 'form.error':
-            makeInvaildText(urlInput, paragraph, i18n.t(current));
-            break;
+  switch (path) {
+    case 'form.error':
+      makeInvaildText(urlInput, paragraph, i18n.t(current));
+      break;
 
-        case 'status':
-            makeSuccesText(urlInput, paragraph, i18n.t('validUrl'));
-            break;
+    case 'status':
+      makeSuccesText(urlInput, paragraph, i18n.t('validUrl'));
+      break;
 
-        default:
-            throw new Error('BOOM!');
-    }
+    default:
+      throw new Error('BOOM!');
+  }
 });
